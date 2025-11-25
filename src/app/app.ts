04584f7a0bebb5ff73627e694716c74f138db87c
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-11-25 12:05:24 +0000

    feat: init project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { OrdersListComponent } from './orders/orders-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [OrdersListComponent],
+  template: `<app-orders-list />`,
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('x-api-training');
-}
+export class App {}
+
