c606d83eeee1c25255f70b82c473483be9bfc448
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-12-07 20:19:15 +0000

    feat: optimistic response

--- src/app/graphql/services/order-graphql.service.ts
+++ src/app/graphql/services/order-graphql.service.ts
@@ -4,16 +4,19 @@ import { Observable } from 'rxjs';
 import {
   IApproveOrderGQL,
   IApproveOrderMutation,
+  IDemoCustomerOrderType,
   IInputApproveCustomerOrderCommand,
   IInputRejectCustomerOrderCommand,
   IPendingForApprovalGQL,
   IPendingForApprovalQuery,
   IRejectOrderGQL,
   IRejectOrderMutation,
+  OrderDetailsFragmentDoc,
 } from '../types/generated';
 
 @Injectable({ providedIn: 'root' })
 export class OrderGraphqlService {
+  private readonly apollo = inject(Apollo);
   private readonly pendingForApprovalGQL = inject(IPendingForApprovalGQL);
   private readonly approveOrderGQL = inject(IApproveOrderGQL);
   private readonly rejectOrderGQL = inject(IRejectOrderGQL);
@@ -27,16 +30,52 @@ export class OrderGraphqlService {
   approveOrder(
     command: IInputApproveCustomerOrderCommand,
   ): Observable<Apollo.MutateResult<IApproveOrderMutation>> {
+    const currentOrder = this.readOrderFromCache(command.orderId);
+
     return this.approveOrderGQL.mutate({
       variables: { command },
+      optimisticResponse: currentOrder ? {
+        approveOrder: {
+          __typename: 'DemoCustomerOrderType',
+          id: command.orderId,
+          status: 'Approved',
+          isApproved: true,
+          storeId: currentOrder.storeId,
+        },
+      } : undefined,
     });
   }
 
   rejectOrder(
     command: IInputRejectCustomerOrderCommand,
   ): Observable<Apollo.MutateResult<IRejectOrderMutation>> {
+    const currentOrder = this.readOrderFromCache(command.orderId);
+
     return this.rejectOrderGQL.mutate({
       variables: { command },
+      optimisticResponse: currentOrder ? {
+        rejectOrder: {
+          __typename: 'DemoCustomerOrderType',
+          id: command.orderId,
+          status: 'Rejected',
+          isApproved: false,
+          storeId: currentOrder.storeId,
+        },
+      } : undefined,
+    });
+  }
+
+  private readOrderFromCache(orderId: string): IDemoCustomerOrderType | null {
+    const id = this.apollo.client.cache.identify({ __typename: 'DemoCustomerOrderType', id: orderId });
+
+    if (!id) {
+      return null;
+    }
+
+    return this.apollo.client.readFragment<IDemoCustomerOrderType>({
+      id,
+      fragment: OrderDetailsFragmentDoc,
+      fragmentName: 'OrderDetails',
     });
   }
 }
