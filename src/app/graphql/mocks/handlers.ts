8f3185fbee39b75132fe4058fe61e7f873950319
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-12-07 21:18:02 +0000

    feat: mocking graphql

---
+++ src/app/graphql/mocks/handlers.ts
@@ -0,0 +1,45 @@
+import { graphql, HttpResponse } from "msw";
+
+export const handlers = [
+  graphql.query("PendingForApproval", () => {
+    return HttpResponse.json({
+      data: {
+        pendingForApprovals: {
+          totalCount: 2,
+          items: [
+            {
+              id: "1",
+              isApproved: true,
+              status: "Approved",
+              storeId: "Electronics",
+              __typename: "DemoCustomerOrderType"
+            },
+            {
+              id: "2",
+              isApproved: false,
+              status: "Rejected",
+              storeId: "Electronics",
+              __typename: "DemoCustomerOrderType"
+            }
+          ],
+          __typename: "DemoCustomerOrderConnection"
+        }
+      }
+    });
+  }),
+
+  graphql.mutation("ApproveOrder", ({ variables }) => {
+    const { command } = variables;
+    return HttpResponse.json({
+      data: {
+        approveOrder: { id: command.orderId, isApproved: true, status: "Approved", storeId: "Electronics", __typename: "DemoCustomerOrderType" }
+      },
+      // errors: [
+      //   {
+      //     message: "Order not found",
+      //     path: ["approveOrder"],
+      //   }
+      // ]
+    });
+  }),
+];
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { setupWorker } from "msw/browser";
+import { handlers } from "./app/graphql/mocks/handlers";
+
+const worker = setupWorker(...handlers);
+await worker.start();
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
